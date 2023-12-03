44fba0bf19d93261c1555b6a471559d2526ef070
Roman Serafymovych <serafymovychr@gmail.com>
2023-12-03 14:59:21 +0300

    first delivery to check

--- bot.py
+++ bot.py
@@ -1,31 +1,37 @@
 contacts = {}
 
+
 def parse_input(user_input):
     cmd, *args = user_input.split()
     cmd = cmd.strip().lower()
     return cmd, *args
+
 
 def add_contact(args, contacts):
     name, phone = args
     contacts[name] = phone
     return "Contact added."
 
+
 def change_contact(args, contacts):
     name, phone = args
     contacts[name] = phone
     return "Contact changed."
+
 
 def phone_contact(args, contacts):
     name = args[0]
     contact_phone = contacts[name]
     return contact_phone
 
+
 def all_contact():
     global contacts
-    all_contacts = ''
+    all_contacts = ""
     for name, phone in contacts.items():
-        all_contacts = all_contacts + name + ' ' + phone + '\n'
+        all_contacts = all_contacts + name + " " + phone + "\n"
     return all_contacts[:-1]
+
 
 def main():
     global contacts
@@ -49,6 +55,7 @@ def main():
             print(all_contact())
         else:
             print("Invalid command.")
+
 
 if __name__ == "__main__":
     main()
--- get_birthdays_per_week.py
+++ get_birthdays_per_week.py
@@ -3,50 +3,56 @@ from collections import defaultdict
 
 
 def get_birthdays_per_week(colleagues_list):
-
-    birthdays_per_week = {'Monday' : [], 'Tuesday' : [], 'Wednesday' : [], 'Thursday' : [], 'Friday' : []}
+    birthdays_per_week = {
+        "Monday": [],
+        "Tuesday": [],
+        "Wednesday": [],
+        "Thursday": [],
+        "Friday": [],
+    }
 
     today = datetime.today().date()
 
-    week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Monday', 'Monday']
+    week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Monday", "Monday"]
 
-    string_full = ''
+    string_full = ""
 
     for colleague in colleagues_list:
-
         name = colleague["name"]
 
         birthday = colleague["birthday"].date()
 
-        birthday_this_year = birthday.replace(year = datetime.today().year)
+        birthday_this_year = birthday.replace(year=datetime.today().year)
 
         if birthday_this_year < today:
-
-            birthday_this_year = birthday_this_year.replace(year = datetime.today().year + 1)
+            birthday_this_year = birthday_this_year.replace(
+                year=datetime.today().year + 1
+            )
 
         if (birthday_this_year - today).days < 7:
-
             birthdays_per_week[week[birthday_this_year.weekday()]].append(name)
 
     for day, names in birthdays_per_week.items():
-
         if names:
-
-            string = day + ': '
+            string = day + ": "
 
             for name in names:
-
                 string = string + name
 
                 if names.index(name) < len(names) - 1:
-
-                    string = string + ', '
+                    string = string + ", "
 
                 else:
-                    
                     string_full = string_full + string + "\n"
-    
+
     return print(string_full[:-1])
 
 
-# get_birthdays_per_week([{"name": "Bill Gates", "birthday": datetime(1955, 12, 5)}, {"name": "Elon Musk", "birthday": datetime(1966, 12, 6)}, {"name": "Madonna", "birthday": datetime(1970, 12, 5)}, {"name": "Bill Murrey", "birthday": datetime(1970, 12, 3)}, {"name": "Jeff Frisker", "birthday": datetime(1970, 12, 2)}, {"name": "Elen Glister", "birthday": datetime(1970, 11, 2)}, {"name": "Viven Pfafer", "birthday": datetime(1970, 12, 25)}])
+# get_birthdays_per_week([{"name": "Bill Gates", "birthday": datetime(1955, 12, 5)}, 
+#                        {"name": "Elon Musk", "birthday": datetime(1966, 12, 6)}, 
+#                        {"name": "Madonna", "birthday": datetime(1970, 12, 5)}, 
+#                        {"name": "Bill Murrey", "birthday": datetime(1970, 12, 3)}, 
+#                        {"name": "Jeff Frisker", "birthday": datetime(1970, 12, 2)}, 
+#                        {"name": "Elen Glister", "birthday": datetime(1970, 11, 2)}, 
+#                        {"name": "Viven Pfafer", "birthday": datetime(1970, 12, 25)},
+#                        ])
